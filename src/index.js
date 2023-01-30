a96f44243b58059dc5b849a1d563f64cd92e397f
Miran-Mirantee <killerm11805@hotmail.com>
2023-01-31 02:12:55 +0700

    Display basic weather report on html

--- src/index.js
+++ src/index.js
@@ -2,18 +2,35 @@ import "./style.css";
 
 const api = "514e1ece08bcd2e992e2242256b805de";
 let city = "bangkok";
+let weather;
+let temperature;
 
-const fetchWeather = async () => {
+async function fetchWeather(cityName) {
   try {
     const response = await fetch(
-      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api}`,
+      `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${api}`,
       { mode: "cors" }
     );
     const weatherInfo = await response.json();
-    console.log(weatherInfo);
+    return weatherInfo;
   } catch (err) {
-    alert("oh no!");
+    alert(err);
+    return "err";
   }
-};
+}
 
-fetchWeather();
+const cityDOM = document.querySelector(".city");
+const weatherDOM = document.querySelector(".main");
+const tempDOM = document.querySelector(".temp");
+
+(async () => {
+  const fetchedWeather = await fetchWeather(city);
+  temperature = fetchedWeather.main.temp;
+  weather = fetchedWeather.weather[0].main;
+
+  cityDOM.textContent = city;
+  weatherDOM.textContent = weather;
+  tempDOM.textContent = temperature - 273.15;
+
+  console.log(fetchedWeather);
+})();
