6a03c46195809cc860329a6fb1e72774876355c4
Miran-Mirantee <killerm11805@hotmail.com>
2023-02-02 00:57:08 +0700

    Change temperature unit

--- src/index.js
+++ src/index.js
@@ -2,6 +2,19 @@ import "./style.css";
 
 const api = "514e1ece08bcd2e992e2242256b805de";
 let city = "bangkok";
+let tempUnit = "c";
+
+const cityDOM = document.querySelector(".city");
+const weatherDOM = document.querySelector(".main");
+const weatherDescDOM = document.querySelector(".desc");
+const tempDOM = document.querySelector(".temp");
+const realFeelDom = document.querySelector(".real-feel");
+const humidityDOM = document.querySelector(".humidity");
+const pressureDOM = document.querySelector(".pressure");
+const windSpeedDOM = document.querySelector(".wind-speed");
+const cityInput = document.getElementById("search-city");
+const form = document.querySelector("form");
+const errorMsg = document.querySelector(".error");
 
 const fetchWeather = async (cityName) => {
   try {
@@ -16,17 +29,17 @@ const fetchWeather = async (cityName) => {
   }
 };
 
-const cityDOM = document.querySelector(".city");
-const weatherDOM = document.querySelector(".main");
-const weatherDescDOM = document.querySelector(".desc");
-const tempDOM = document.querySelector(".temp");
-const realFeelDom = document.querySelector(".real-feel");
-const humidityDOM = document.querySelector(".humidity");
-const pressureDOM = document.querySelector(".pressure");
-const windSpeedDOM = document.querySelector(".wind-speed");
-const cityInput = document.getElementById("search-city");
-const form = document.querySelector("form");
-const errorMsg = document.querySelector(".error");
+const convertTemp = (unit, temp) => {
+  // celsius
+  if (unit === "c") {
+    return `${Math.round((temp - 273.15) * 100) / 100}°C`;
+  }
+  // fahrenheit
+  if (unit === "f") {
+    return `${Math.round(((temp - 273.15) * 1.8 + 32) * 100) / 100}°F`;
+  }
+  return "error";
+};
 
 const updateWeather = async () => {
   try {
@@ -34,7 +47,7 @@ const updateWeather = async () => {
     const { name } = weatherInfo;
     const weather = weatherInfo.weather[0].main;
     const desc = weatherInfo.weather[0].description;
-    const temperature = weatherInfo.main.temp;
+    const { temp } = weatherInfo.main;
     const realFeel = weatherInfo.main.feels_like;
     const { humidity } = weatherInfo.main;
     const { pressure } = weatherInfo.main;
@@ -43,10 +56,8 @@ const updateWeather = async () => {
     cityDOM.textContent = name;
     weatherDOM.textContent = weather;
     weatherDescDOM.textContent = desc;
-    tempDOM.textContent = `${Math.round((temperature - 273.15) * 100) / 100}°C`;
-    realFeelDom.textContent = `Feels like: ${
-      Math.round((realFeel - 273.15) * 100) / 100
-    }°C`;
+    tempDOM.textContent = convertTemp(tempUnit, temp);
+    realFeelDom.textContent = convertTemp(tempUnit, realFeel);
     humidityDOM.textContent = `Humidity: ${humidity}%`;
     pressureDOM.textContent = `Pressure: ${pressure} hPa`;
     windSpeedDOM.textContent = `Wind Speed: ${windSpeed} m/s`;
@@ -61,5 +72,14 @@ updateWeather();
 form.addEventListener("submit", () => {
   city = cityInput.value;
   cityInput.value = "";
+  updateWeather();
+});
+
+tempDOM.addEventListener("click", () => {
+  if (tempUnit === "c") {
+    tempUnit = "f";
+  } else {
+    tempUnit = "c";
+  }
   updateWeather();
 });
